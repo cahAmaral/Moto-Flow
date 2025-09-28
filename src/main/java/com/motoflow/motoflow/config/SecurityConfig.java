3ca7378e9b13a9547dfaaaeb3e0f812bf2018cc3
cahAmaral <cahxss@gmail.com>
2025-09-28 13:21:37 -0300

    desativando security

---
+++ src/main/java/com/motoflow/motoflow/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.motoflow.motoflow.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        return username -> null;
+    }
+}
